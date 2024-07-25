975745dd531e7606df24c9fd3650f58a6f69580d
Piyush-0605 <goluchoudhari411@gmail.com>
2024-07-25 23:50:03 +0530

    changes

--- src/Components/NewsBoads/NewsBoads.jsx
+++ src/Components/NewsBoads/NewsBoads.jsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import './NewsBoads.css'
 import NewsItem from '../NewsItem/NewsItem';
+import axios from 'axios';
 
 const NewsBoads = ({category}) => {
-    const[articles,setArticles]=useState([]);
+    const[aarticles,setAarticles]=useState([]);
 
     useEffect(()=>{  
         let url=`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=7e0b90f160234b009686365e6b031e90`;
+
+      
         fetch(url,{
-          method: 'GET',
           headers: {
-            'Upgrade': 'HTTP/2.0'
-          }
+            "Upgrade": "HTTP/2.0"
+        }
+        
         })
-        .then(res=>res.json())
-        .then((data)=>setArticles(data.articles));
+        .then(res=>{
+          if (!res.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return res.json();
+        })
+        .then((data)=>setAarticles(data.articles))
+        .catch(error => console.error('There has been a problem with your fetch operation:', error));
+       
     },[category])
+    console.log(aarticles)
 
    
   return (
@@ -23,7 +34,7 @@ const NewsBoads = ({category}) => {
       <h3>Latest<span>News</span></h3>
       <div className='newsitem-data'>
       {
-        articles && articles.map((news,index)=>{
+        aarticles && aarticles.map((news,index)=>{
             return(
                 <NewsItem key={index}
                     title={news.title}
